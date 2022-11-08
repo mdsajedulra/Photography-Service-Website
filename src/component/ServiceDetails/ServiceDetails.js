639b902b426e8f29d20618ec06b3bc062f40a570
mdsajedulra <mdsajedulra@gmail.com>
2022-11-08 18:46:57 +0600

    review form working

--- src/component/ServiceDetails/ServiceDetails.js
+++ src/component/ServiceDetails/ServiceDetails.js
@@ -3,13 +3,37 @@ import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const services = useLoaderData();
-    const { _id, img, title, description } = services;
+    const { _id, img, title, description, price } = services;
     console.log(services)
     return (
         <div className='w-1/2 mx-auto my-10'>
             <img className='w-full text-center rounded-xl' src={img} alt="" />
-            <h1 className="my-3 text-2xl font-semibold">{title}</h1>
+            <div className='p-3 my-5 bg-blue-200 border border-blue-700 rounded-lg'>
+                <h1 className="text-2xl font-semibold ">{title}</h1>
+                <h1>Price: {price}</h1>
+            </div>
             <p>{description}</p>
+
+            {/* Reviews section */}
+
+            <div>
+                <h1 className='text-3xl'>Review Us</h1>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <br />
+                    <input className='w-full border rounded-xl' type="text" placeholder='Name' id='name' />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <br />
+                    <input className='w-full border rounded-xl' id='email' type="email" placeholder='Email' />
+                </div>
+                <div>
+                    <label htmlFor="comment">Comment</label>
+                    <br />
+                    <input className='w-full border rounded-xl' id='comment' type="text" placeholder='Write your comment here' />
+                </div>
+            </div>
         </div>
     );
 };
